992409b15b9762bb470d1b85ce9d9353bb289434
jakubwro <jakubwro@gmail.com>
2020-01-24 20:01:26 +0100

    switch to pytest

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,40 +1,19 @@
-import unittest
+import pytest
 from src.lexer import Lexer
 
 def alltokens(lexer):
     while (lexer.peek() != None):
         yield lexer.next()
 
-class TestLexer(unittest.TestCase):
-
-    def test_empty_input(self):
-        lexer = Lexer("")
-        self.assertEqual(lexer.peek(), None)
-
-    def test_empty_list(self):
-        lexer = Lexer("()")
-        tokens = list(alltokens(lexer))
-        self.assertEqual(tokens, ['(', ')'])
-
-    def test_missing_closing_paren(self):
-        lexer = Lexer("(+ 3 4")
-        tokens = list(alltokens(lexer))
-        self.assertEqual(tokens, ['(', '+', '3', '4'])
-
-    def test_nil_token(self):
-        lexer = Lexer("(nil)")
-        tokens = list(alltokens(lexer))
-        self.assertEqual(tokens, ['(', 'nil', ')'])
-
-    def test_booleans(self):
-        lexer = Lexer("(true false)")
-        tokens = list(alltokens(lexer))
-        self.assertEqual(tokens, ['(', 'true', 'false', ')'])
-
-    def test_numbers(self):
-        lexer = Lexer("(1 2 3.14 -4 -5.0)")
-        tokens = list(alltokens(lexer))
-        self.assertEqual(tokens, ['(', '1', '2', '3.14', '-4', '-5.0', ')'])
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize("expression,expected", [
+    ("", []),
+    ("()", ['(', ')']),
+    ("(+ 3 4",  ['(', '+', '3', '4']),
+    ("(nil)", ['(', 'nil', ')']),
+    ("(true false)", ['(', 'true', 'false', ')']),
+    ("(1 2 3.14 -4 -5.0)", ['(', '1', '2', '3.14', '-4', '-5.0', ')'])
+])
+def test_lexer(expression, expected):
+    lexer = Lexer(expression)
+    actual = list(alltokens(lexer))
+    assert actual == expected
